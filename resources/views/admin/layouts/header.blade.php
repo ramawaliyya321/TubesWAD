99c01e3b171cce32526321a7f5b4d435dffabd24
ramawaliyya321 <ramawaliyya@gmail.com>
2023-12-30 19:50:11 +0700

    Sidebar title updated

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -21,7 +21,7 @@
         <div class="col-md-3 left_col">
             <div class="left_col scroll-view">
                 <div class="navbar nav_title" style="border: 0;">
-                    <a href="{{ url('dashboard') }}" class="site_title"><i class="fa fa-book"></i> <span>{{ $settings['app_name'] or 'E-Learning' }}</span></a>
+                    <a href="{{ url('dashboard') }}" class="site_title"><i class="fa fa-book"></i> <span>{{'E-Learning' }}</span></a>
                 </div>
 
                 <div class="clearfix"></div>
